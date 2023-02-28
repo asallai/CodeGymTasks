727704bf0891f0605ce8917fe7ff0fb7d6b7885f
Aniko Sallai <aniko.sallai@gmail.com>
2023-02-28 22:54:13 +0100

    Level 6

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task05/task0507/Solution.java
@@ -0,0 +1,32 @@
+package en.codegym.task.pro.task05.task0507;
+
+import java.util.Scanner;
+
+/* 
+Maximum of N numbers
+*/
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        array = new int[n];
+
+        for(int i=0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int max = array[0];
+
+        for(int i=1; i < array.length; i++) {
+            if(array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
